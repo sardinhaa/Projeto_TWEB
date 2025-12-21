eb9bdfd46a8b3a89054ef7caebc038d8f07fecaf
gonçalo Pereira <128597095+sardinhaa@users.noreply.github.com>
2025-12-21 12:02:26 +0000

    Add files via upload

---
+++ Ilha Verde_arquivos/custom.js
@@ -0,0 +1,53 @@
+// (function () {
+	
+	// const loadingGoogleAnalyticsData = () => {
+			// (function(){
+				// const s = document.createElement('script');
+				// s.setAttribute('src', 'https://www.googletagmanager.com/gtag/js?id=G-YKQPTVG52X');
+				// s.async = true;
+				// document.head.appendChild(s);
+
+				// window.dataLayer = window.dataLayer || [];
+				// window.gtag = function() {
+				  // window.dataLayer.push(arguments);
+				// };
+
+				// window.gtag('js', new Date());
+				// window.gtag('config', 'G-YKQPTVG52X');
+			// }());
+	// }
+	// window.addEventListener('load',() => setTimeout(loadingGoogleAnalyticsData,0),false);
+	
+// }());
+// (function () {
+  // const loadGA = () => {
+    // const s = document.createElement('script');
+    // s.setAttribute('src', 'https://www.googletagmanager.com/gtag/js?id=G-YKQPTVG52X');
+    // s.async = true;
+    // document.head.appendChild(s);
+
+    // window.dataLayer = window.dataLayer || [];
+    // window.gtag = function () {
+      // window.dataLayer.push(arguments);
+    // };
+
+    // window.gtag('js', new Date());
+    // window.gtag('config', 'G-YKQPTVG52X');
+  // };
+
+  // const loadGTM = () => {
+    // const gtmId = 'GTM-P7FHS5C8'; // replace with your actual GTM ID
+    // window.dataLayer = window.dataLayer || [];
+    // window.dataLayer.push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+
+    // const gtmScript = document.createElement('script');
+    // gtmScript.async = true;
+    // gtmScript.src = `https://www.googletagmanager.com/gtm.js?id=${gtmId}`;
+    // document.head.appendChild(gtmScript);
+  // };
+
+  // window.addEventListener('load', () => {
+    // loadGA();
+    // loadGTM();
+  // }, false);
+// })();
